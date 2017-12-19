956c9d9fb2ebb96679b04b85079edf0367c6ae22
Danilo Silva <danilosilva@MacBook-Air-de-Danilo.local>
2017-12-19 21:40:56 -0200

    modal adicionado faltando apenas seguir a especificacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { DialogComponent } from './dialog/dialog.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DialogComponent
+    DialogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
